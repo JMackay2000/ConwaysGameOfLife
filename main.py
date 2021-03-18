340d206d4ad86fab977e93711676817ab7b11c8b
JMackay2000 <jordanmackay174@gmail.com>
2021-03-18 15:11:57 +0800

    Resizable Windows and Refactoring
    Added resizable windows, refactored code to make more readable

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 import pygame
 from datetime import datetime
 
-from constants import *
+BLACK = (0,0,0,1)
+WHITE = (255,255,255,1)
 
 class Game(object):
 
@@ -12,13 +13,20 @@ class Game(object):
         self.surface: pygame.Surface = None
         self.__running = False
         self.__sim_running = False
-        self.cells = [] # 0=dead, 1=alive
+        self.cells = [] # 0 is dead, 1 is alive
+        self.foods = [] # between 1-15
+        self.WIDTH = 800
+        self.HEIGHT = 800
+        self.CW = 10
+        self.CH = 10
+
 
     def init_game(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.surface = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
         pygame.display.set_caption('Conways Game of Life')
         self.__init_cells()
+
         self.__running = True
 
 
@@ -33,9 +41,10 @@ class Game(object):
             lag += elapsed
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.__running = False
-                    pygame.quit()
+                    self.__quit()
                     return
+                elif event.type == pygame.VIDEORESIZE:
+                    self.__resize(event.w, event.h)
                 self.__input(event)
 
             
@@ -57,50 +66,51 @@ class Game(object):
             if not self.__sim_running:
                 if event.key == pygame.K_s:
                     self.__save_cell_state_to_disk()
-                if event.key == pygame.K_l:
+                elif event.key == pygame.K_l:
                     self.__load_cell_state_from_disk()
-                if event.key == pygame.K_r:
+                elif event.key == pygame.K_r:
                     self.__init_cells()
-            
-        if not self.__sim_running:
-            if pygame.mouse.get_pressed():
-                pos = pygame.mouse.get_pos()
-                # calc rect that was pressed
-                ry = pos[1] % CH
-                rx = pos[0] % CW
-                y = (pos[1] - ry) 
-                x = (pos[0] - rx) 
-                if 0 <= x <= WIDTH and 0 <= y <= HEIGHT:
-                    ix = int(x/CW)
-                    iy = int(y/CH)
-                    state = self.cells[iy][ix]
-                    if pygame.mouse.get_pressed()[0]: # left click
-                        self.cells[iy][ix] = 1
-                    elif pygame.mouse.get_pressed()[2]: # right click
-                        self.cells[iy][ix] = 0
+                elif event.key == pygame.K_q:
+                    self.__quit()
+        
+        # draw cells with mouse if simulation isn't running
+        if not self.__sim_running and pygame.mouse.get_pressed():
+            pos = pygame.mouse.get_pos()
+            # calc rect that was pressed
+            ry = pos[1] % self.CH
+            rx = pos[0] % self.CW
+            y = (pos[1] - ry) 
+            x = (pos[0] - rx) 
+            if 0 <= x <= self.WIDTH and 0 <= y <= self.HEIGHT:
+                ix = int(x/self.CW)
+                iy = int(y/self.CH)
+                if pygame.mouse.get_pressed()[0]: # left click
+                    self.cells[iy][ix] = 1
+                elif pygame.mouse.get_pressed()[2]: # right click
+                    self.cells[iy][ix] = 0
 
 
     def __update(self):
-        for y in range(int(HEIGHT/CH)):
-            for x in range(int(WIDTH/CW)):
+        for y in range(int(self.HEIGHT/self.CH)):
+            for x in range(int(self.WIDTH/self.CW)):
                 ul = um = ur = ml = mr = ll = \
                     lm = lr = 0
                 if y > 0:
                     if x > 0:
                         ul = self.cells[y-1][x-1]
                     um = self.cells[y-1][x]
-                    if x < (WIDTH/CW)-1:
+                    if x < (self.WIDTH/self.CW)-1:
                         ur = self.cells[y-1][x+1]
 
                 if x > 0:
                     ml = self.cells[y][x-1]
-                if x < (WIDTH/CW)-1:
+                if x < (self.WIDTH/self.CW)-1:
                     mr = self.cells[y][x+1]
                 
-                if y < int(HEIGHT/CH) - 1:
+                if y < int(self.HEIGHT/self.CH) - 1:
                     if x > 0:
                         ll = self.cells[y+1][x-1]
-                    if x < (WIDTH/CW)-1:
+                    if x < (self.WIDTH/self.CW)-1:
                         lr = self.cells[y+1][x+1]
                     lm = self.cells[y+1][x]
                 
@@ -115,9 +125,9 @@ class Game(object):
 
 
     def __render(self):
-        for y in range(int(HEIGHT/CH)):
-            for x in range(int(WIDTH/CW)):
-                rect = pygame.Rect(x*CW,y*CH,CW,CH)
+        for y in range(int(self.HEIGHT/self.CH)):
+            for x in range(int(self.WIDTH/self.CW)):
+                rect = pygame.Rect(x*self.CW,y*self.CH,self.CW,self.CH)
                 if self.cells[y][x] == 0:
                     colour = BLACK
                 else:
@@ -126,19 +136,16 @@ class Game(object):
 
 
     def __init_cells(self):
-        # rand = random.Random()
-        # rand.seed(datetime.now().microsecond)
         self.cells = list()
-        for y in range(int(HEIGHT/CH)):
+        for y in range(int(self.HEIGHT/self.CH)):
             self.cells.append(list())
-            for x in range(int(WIDTH/CW)):
-                # state = rand.randint(0, 1)
+            for x in range(int(self.WIDTH/self.CW)):
                 state = 0
                 self.cells[y].append(state)
 
 
     def __save_cell_state_to_disk(self):
-        s = f'{WIDTH} {HEIGHT}\n'
+        s = f'{self.WIDTH} {self.HEIGHT}\n'
         name = str(input("enter name of save: "))
         for row in self.cells:
             for col in row:
@@ -149,7 +156,7 @@ class Game(object):
                 f.write(s)
                 f.close()
             return True
-        except:
+        except IOError:
             return False
     
 
@@ -158,11 +165,26 @@ class Game(object):
         name = str(input("name of save: "))
         with open(f'{name}.txt', 'r') as f:
             l1 = f.readline().strip('\n').split(' ')
-            WIDTH = int(l1[0])
-            HEIGHT = int(l1[1])
+            self.WIDTH = int(l1[0])
+            self.HEIGHT = int(l1[1])
             for line in f.readlines():
                 line = line.strip('\n').split(' ')[:-1]
                 self.cells.append([int(c) for c in line])
+
+
+    def __resize(self, new_width: int, new_height: int):
+        sfh = new_height / self.HEIGHT
+        sfw = new_width / self.WIDTH
+        self.WIDTH *= sfw
+        self.HEIGHT *= sfh
+        self.CW *= sfw
+        self.CH *= sfh
+        self.surface = pygame.display.set_mode((int(self.WIDTH), int(self.HEIGHT)), pygame.RESIZABLE)
+
+
+    def __quit(self):
+        self.__running = False
+        pygame.quit()
 
 
 if __name__ == '__main__':
